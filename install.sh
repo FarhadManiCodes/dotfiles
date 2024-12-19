f025b137e0ca91d33ccce731345dff32d6d5b379
Farhad Mani <frhdmani@gmail.com>
2024-12-19 14:36:50 +0100

    update changes for zsh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 # vim
 ln -sf "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
 # zsh
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+# ptpyhthon
 mkdir -p "$HOME/.config/ptpython"
 ln -sf "$HOME/dotfiles/ptpython/config.py" "$HOME/.config/ptpython/config.py"
---
+++ zsh/.zshenv
@@ -0,0 +1,11 @@
+# For dotfiles
+export XDG_CONFIG_HOME="$HOME/.config"
+# For specific data
+export XDG_DATA_HOME="$XDG_CONFIG_HOME/local/share"
+# For cached files
+export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
+
+export EDITOR="vim"
+export VISUAL="vim"
+
+export DOTFILES="$HOME/dotfiles"
