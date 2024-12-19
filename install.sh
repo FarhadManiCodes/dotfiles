9dc757a541c1e56b3f504a6a409438901509e828
Farhad Mani <frhdmani@gmail.com>
2024-12-19 15:43:31 +0100

    add basic tmux settings

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+# tmux
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+
 # ptpyhthon
 mkdir -p "$HOME/.config/ptpython"
 ln -sf "$HOME/dotfiles/ptpython/config.py" "$HOME/.config/ptpython/config.py"
