ce92600815adf86402dbb9cb0ddb7eed03f78812
Farhad Mani <frhdmani@gmail.com>
2024-12-19 14:25:19 +0100

    Add zsh config

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# vim
 ln -sf "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
+# zsh
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 mkdir -p "$HOME/.config/ptpython"
 ln -sf "$HOME/dotfiles/ptpython/config.py" "$HOME/.config/ptpython/config.py"
