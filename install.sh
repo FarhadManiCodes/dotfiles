bc1011748742961c869f0323a4995c676a4e1250
Farhad Mani <frhdmani@gmail.com>
2025-05-15 20:58:28 +0200

    modify the address for the tmux config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
 # tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ln -sf "$HOME/dotfiles/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 
 # ptpyhthon
 mkdir -p "$HOME/.config/ptpython"
