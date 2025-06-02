e832864039ffa1b5dae56cdc847a125b3c673b19
Farhad Mani <frhdmani@gmail.com>
2025-06-02 14:13:34 +0200

    add foot terminal setting to the dotfile

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ ln -sf "$HOME/dotfiles/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 mkdir -p "$HOME/.config/ptpython"
 ln -sf "$HOME/dotfiles/ptpython/config.py" "$HOME/.config/ptpython/config.py"
 
+# foot terminal
+mkdir -p "$XDG_CONFIG_HOME/foot"
+ln -sf "~/dotfiles/foot/foot.ini" "$XDG_CONFIG_HOME/foot/foot.ini"
 #########
 # Fonts #
 #########
