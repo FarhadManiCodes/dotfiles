fa2ceab36b02fc7bc19bd9e62c54da6c45c70784
Farhad Mani <frhdmani@gmail.com>
2025-06-09 20:27:35 +0200

    Update vim instalation

--- dev-env/runs/vim.sh
+++ dev-env/runs/vim.sh
@@ -12,9 +12,9 @@ INSTALL_FORLDER="$HOME/install/vim"
 # Install required dependencies
 sudo apt update
 sudo apt install -y \
-  build-essential ncurses-dev python3-dev git curl\
+  build-essential ncurses-dev python3-dev python3-venv git curl\
   libtool libtool-bin autoconf automake cmake g++ pkg-config unzip \
-  libx11-dev libxt-dev wl-clipboard libncursesw5-dev 
+  libx11-dev libxt-dev wl-clipboard libncursesw5-dev llvm
 
 # Clone Vim source code
 git clone --depth 1 https://github.com/vim/vim.git "$INSTALL_FORLDER"
@@ -47,3 +47,38 @@ echo "Install plugin manager"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -E -u ~/.vimrc +PlugInstall +qa
+
+# colorscheme installation
+
+echo "Add one dark as the colorsheme"
+# Define target paths
+VIM_COLORS="$HOME/.vim/colors"
+VIM_AUTOLOAD="$HOME/.vim/autoload"
+LIGHTLINE_SCHEME="$HOME/.vim/autoload/lightline/colorscheme"
+
+# Create required directories
+mkdir -p "$VIM_COLORS"
+mkdir -p "$VIM_AUTOLOAD"
+mkdir -p "$LIGHTLINE_SCHEME"
+
+# Download the main onedark colorscheme
+echo "Downloading onedark.vim color scheme..."
+curl -fsSL -o "$VIM_COLORS/onedark.vim" \
+  https://raw.githubusercontent.com/joshdick/onedark.vim/main/colors/onedark.vim
+
+# Download the autoload support file
+echo "Downloading autoload/onedark.vim..."
+curl -fsSL -o "$VIM_AUTOLOAD/onedark.vim" \
+  https://raw.githubusercontent.com/joshdick/onedark.vim/main/autoload/onedark.vim
+
+# Download the lightline colorscheme
+echo "Downloading Lightline onedark theme..."
+curl -fsSL -o "$LIGHTLINE_SCHEME/onedark.vim" \
+  https://raw.githubusercontent.com/joshdick/onedark.vim/main/autoload/lightline/colorscheme/onedark.vim
+
+echo "Installation complete."
+echo "Remember to set the colorscheme and Lightline theme manually in your .vimrc:"
+echo ""
+echo "    colorscheme onedark"
+echo "    let g:lightline = { 'colorscheme': 'onedark' }"
+echo ""
